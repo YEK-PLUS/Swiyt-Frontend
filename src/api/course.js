74f7e04a3a29804e09e071d0ac407ff2f65f6172
YEK-PLUS <admin@f4med.com>
2019-09-08 04:04:14 +0300

    added cubscriptions

--- src/api/course.js
+++ src/api/course.js
@@ -4,3 +4,8 @@ export const PopulerCourse = async () => {
   const response = await API('post', '/course/populer');
   return response;
 };
+export const Subscriptions = async () => {
+  const response = await API('post', '/course/subscriptions');
+  return response;
+};
+export default { PopulerCourse,Subscriptions };
