f88f8cfc9a4f2df1ead44b16959b79bb13292888
YEK-PLUS <admin@f4med.com>
2019-09-11 20:41:12 +0300

    re-edited for null userdetail value state

--- src/containers/publicPage.js
+++ src/containers/publicPage.js
@@ -24,14 +24,22 @@ class publicProfile extends React.Component {
       <div className="w-full">
         <div className="py-4 px-16 mx-auto">
           <Head data={this.state.data}/>
-          <About about={this.state.data.user_details.biography}/>
-          <Education education={this.state.data.user_details.education}/>
-          <Certificate certificate={this.state.data.user_details.certificates}/>
-          <Experience experience={this.state.data.user_details.experience}/>
-          <Referance referance={this.state.data.user_details.referance}/>
+          {this.state.data.user_details?
+            <div>
+              <About about={this.state.data.user_details.biography}/>
+              <Education education={this.state.data.user_details.education}/>
+              <Certificate certificate={this.state.data.user_details.certificates}/>
+              <Experience experience={this.state.data.user_details.experience}/>
+              <Referance referance={this.state.data.user_details.referance}/>
+            </div>
+            :null}
         </div>
-        <Portfoy portfoy={this.state.data.user_details.portfolio}/>
-        <Courses courses={this.state.data.lessons}/>
+        {this.state.data.user_details?
+          <div>
+            <Portfoy portfoy={this.state.data.user_details.portfolio}/>
+            <Courses courses={this.state.data.lessons}/>
+          </div>
+        :null}
       </div>
       :null}
     </div>
