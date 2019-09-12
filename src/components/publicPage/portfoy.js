2bfa0a93a04c827bfa53f3c4741fc86b95568d7a
YEK-PLUS <admin@f4med.com>
2019-09-12 23:30:05 +0300

    slick updated

--- src/components/publicPage/portfoy.js
+++ src/components/publicPage/portfoy.js
@@ -46,7 +46,7 @@ export default class Portfoy extends React.Component {
       return (
         <div className="w-full py-4 bg-gray-200">
           <div className="w-full text-center text-black text-2xl">Portfoy</div>
-          <div className="w-full px-16 overflow-hidden">
+          <div className="w-full px-16 overflow-hidden public-page">
             <Slider {...settings}>
               {this.loadPortfoy().map((a)=>a)}
             </Slider>
