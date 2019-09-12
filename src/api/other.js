7834b9008f5039e0edaf0cf0bc746ab4b4d7b3e2
YEK-PLUS <admin@f4med.com>
2019-09-12 23:29:33 +0300

    add new api endpoint connector -> SwiytComments

--- src/api/other.js
+++ src/api/other.js
@@ -4,4 +4,8 @@ export const References = async () => {
   const response = await API('post', '/main/references');
   return response;
 };
-export default { References };
+export const SwiytComments = async () => {
+  const response = await API('post', '/main/swiytComments');
+  return response;
+};
+export default { References,SwiytComments };
--- src/helper/api/others.js
+++ src/helper/api/others.js
@@ -1,7 +1,11 @@
-import { References } from '../../api/other';
+import { References,SwiytComments } from '../../api/other';
 
 const GetReferences = async () => {
   const response = await References();
   return response;
 };
-export default { GetReferences };
+const GetSwiytComments = async () => {
+  const response = await SwiytComments();
+  return response;
+};
+export default { GetReferences,GetSwiytComments };
