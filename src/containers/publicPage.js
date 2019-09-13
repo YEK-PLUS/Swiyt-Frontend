eaa06b635530f6cda5d5d2b47275631220151ba7
YEK-PLUS <admin@f4med.com>
2019-09-13 21:14:36 +0300

    added user not found method

--- src/containers/publicPage.js
+++ src/containers/publicPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import _ from 'lodash';
 import Helpers from '../helper';
 import {Head,About,Education,Certificate,Experience,Referance,Portfoy,Courses} from '../components/publicPage';
 class publicProfile extends React.Component {
@@ -17,33 +17,46 @@ class publicProfile extends React.Component {
     this.setState({data:userDetails});
     this.setState({loading:false});
   }
-  render(){
+  loadProfile(){
     return (
-    <div>
-      {!this.state.loading?
-      <div className="w-full">
-        <div className="py-4 px-16 mx-auto">
-          <Head data={this.state.data}/>
+      <div>
+        {!this.state.loading?
+        <div className="w-full">
+          <div className="py-4 px-16 mx-auto">
+            <Head data={this.state.data}/>
+            {this.state.data.user_details?
+              <div>
+                <About about={this.state.data.user_details.biography}/>
+                <Education education={this.state.data.user_details.education}/>
+                <Certificate certificate={this.state.data.user_details.certificates}/>
+                <Experience experience={this.state.data.user_details.experience}/>
+                <Referance referance={this.state.data.user_details.referance}/>
+              </div>
+              :null}
+          </div>
           {this.state.data.user_details?
             <div>
-              <About about={this.state.data.user_details.biography}/>
-              <Education education={this.state.data.user_details.education}/>
-              <Certificate certificate={this.state.data.user_details.certificates}/>
-              <Experience experience={this.state.data.user_details.experience}/>
-              <Referance referance={this.state.data.user_details.referance}/>
+              <Portfoy portfoy={this.state.data.user_details.portfolio}/>
+              <Courses courses={this.state.data.lessons}/>
             </div>
-            :null}
+          :null}
         </div>
-        {this.state.data.user_details?
-          <div>
-            <Portfoy portfoy={this.state.data.user_details.portfolio}/>
-            <Courses courses={this.state.data.lessons}/>
-          </div>
         :null}
       </div>
-      :null}
-    </div>
     );
+  }
+  loadError(){
+    return (
+      <div className={`w-full py-3 px-5`}>
+        User Not Found
+      </div>
+    );
+  }
+  render(){
+    if(!(_.has(this.state.data,'error'))){
+      return this.loadProfile();
+    }
+    return this.loadError();
   }
 }
 
